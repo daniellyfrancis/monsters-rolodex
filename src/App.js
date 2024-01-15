fb44bcb4b38224b9bab90a428c53c9ea4aff5bb5
daniellyfrancis <francisdanielly04@gmail.com>
2024-01-15 16:47:21 +0000

    using map method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda',
+          id: 1,
+        },
+        {
+          name: 'Frank',
+          id: 2, 
+        },
+        {
+        name: 'Jacky',
+        id: 3,
+        },
+        {
+        name: 'Thiago',
+        id: 4,
+        }
+      ]
+    }
+  }
+
+  render() {
+    return <div className='App'>
+      {
+        this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>
+        })
+      }
+    </div>;
+  }
 }
 
 export default App;
