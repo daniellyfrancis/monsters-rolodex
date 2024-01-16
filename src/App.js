81cfd59ca40f060055949d8d81926943a08cea64
daniellyfrancis <francisdanielly04@gmail.com>
2024-01-16 10:22:35 +0000

    using componentDidMount()

--- src/App.js
+++ src/App.js
@@ -6,25 +6,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda',
-          id: 1,
-        },
-        {
-          name: 'Frank',
-          id: 2, 
-        },
-        {
-        name: 'Jacky',
-        id: 3,
-        },
-        {
-        name: 'Thiago',
-        id: 4,
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => {
+        return { monsters: users }
+      },
+        () => {
+          console.log(this.state);
         }
-      ]
-    }
+      ));
   }
 
   render() {
