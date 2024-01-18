5caa03c5e310b5cb8cf246ec1dedb4aa73c875c0
daniellyfrancis <francisdanielly04@gmail.com>
2024-01-18 10:04:33 +0000

    add input component

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: []
     };
+    console.log('1')
   }
 
   componentDidMount() {
+    console.log('3')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(() => {
@@ -23,7 +25,9 @@ class App extends Component {
   }
 
   render() {
+    console.log('2')
     return <div className='App'>
+      <input className='search-box' type='search' placeholder='search monsters'/>
       {
         this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>
